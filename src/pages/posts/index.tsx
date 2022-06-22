1f2fee52ceceafd456353aa155d614906e95d0fb
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-06-21 23:44:41 -0300

    feat: componente ActiveLink

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+
+import { getPrismicClient } from '../../services/prismic'
+
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+        slug: string;
+        title: string;
+        excerpt: string;
+        updatedAt: string;
+}
+interface PostProps {
+   posts: Post[]
+}
+
+export default function Posts({ posts }: PostProps) {
     return (
         <>
             <Head>
@@ -10,23 +26,45 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Boas práticas para devs em início de carreira</strong>
-                        <p>As principais lições e dicas compiladas para quem está começando na programação ou migrando para a área</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Boas práticas para devs em início de carreira</strong>
-                        <p>As principais lições e dicas compiladas para quem está começando na programação ou migrando para a área</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Boas práticas para devs em início de carreira</strong>
-                        <p>As principais lições e dicas compiladas para quem está começando na programação ou migrando para a área</p>
-                    </a>
+                    { posts.map(post => (
+                         <a key={post.slug} href="#">
+                         <time>{post.updatedAt}</time>
+                         <strong>{post.title}</strong>
+                         <p>{post.excerpt}</p>
+                     </a>
+                    )) }
                 </div>
             </main>
         </>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query<any>([
+        Prismic.predicates.at('document.type', 'post')
+     ], {
+        fetch: ['post.title', 'post.content'],
+        pageSize: 100,
+     } )
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+
+    return {
+        props: {
+            posts
+        }
+    }
 }
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,9 +1,10 @@
 import Prismic from '@prismicio/client'
 
-export function getPrismicClient() {
-    const prismic = Prismic.createClient(
-        process.env.PRISMIC_ACCESS_TOKEN,
+export function getPrismicClient(req?: unknown) {
+    const prismic = Prismic.client(
+        process.env.PRISMIC_ENDPOINT,
         {
+            req,
             accessToken: process.env.PRISMIC_ACCESS_TOKEN
         }
     )
