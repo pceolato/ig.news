dc33fcf7e6e7c4dc97d13bc3cefe6b63321d5b3a
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-06-22 21:08:28 -0300

    feat: paginas estaticas

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
+import Link from 'next/link'
 
 import { getPrismicClient } from '../../services/prismic'
 
@@ -27,11 +28,15 @@ export default function Posts({ posts }: PostProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     { posts.map(post => (
-                         <a key={post.slug} href="#">
-                         <time>{post.updatedAt}</time>
-                         <strong>{post.title}</strong>
-                         <p>{post.excerpt}</p>
-                     </a>
+                        <Link
+                            key={post.slug}   
+                            href={`/posts/preview/${post.slug}`}>
+                            <a >
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
                     )) }
                 </div>
             </main>
