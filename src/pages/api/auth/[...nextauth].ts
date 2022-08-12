b394485578b3f9da8868609386d8096d0f98ca23
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-08-12 13:34:56 -0300

    refactor: nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,87 +1,97 @@
-import { query as q, query } from "faunadb"
-
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-
+import { query } from 'faunadb'
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
 import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
-    ], 
-    callbacks: {
-        async session({ session }) {
-            try {
-                            
-            const userActiveSubscription = await fauna.query(
-                q.Get(
-                    q.Intersection([
-                        q.Match(
-                            q.Index('subscription_by_user_ref'),
-                            q.Select(
-                                "ref",
-                                q.Get(
-                                    q.Match(
-                                        q.Index('user_by_email'),
-                                        q.Casefold(session.user.email)
-                                    )
-                                )
-                            )
-                        ),
-                        q.Match(
-                            q.Index('subscription_by_status'),
-                            "active"
-                        )               
-                    ])
-                )
-            )
-
-            return {
-                ...session,
-                activeSubscription: userActiveSubscription
-            }
-        } catch {
-            return {
-                ...session,
-                activeSubscription: null,
-                }
-            } 
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user',
         },
-        async signIn({user, account, profile}) {
-            const { email } = user
-            
-            try { 
-                await fauna.query(
-                    q.If(
-                        q.Not(
-                            q.Exists(
-                                q.Match(
-                                    q.Index('user_by_email'),
-                                    q.Casefold(user.email)
-                                )
-                            )
-                        ),
-                        q.Create(
-                            q.Collection('users'),
-                            { data: {email} }
-                        ),
-                        q.Get(
-                            q.Match(
-                                q.Index('user_by_email'),
-                                q.Casefold(user.email)
-                            )
-                        )
+      },
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.NEXT_AUTH_SECRET,
+  callbacks: {
+    async session({ session }) {
+      if (!session.user?.email) {
+        return session
+      }
+
+      try {
+        //buscar se o usuario ja tem uma assinatura
+        const userActiveSubscription = await fauna.query(
+          //retorna a subscription pela ref do usuario
+          query.Get(
+            query.Intersection([
+              query.Match(
+                query.Index('subscription_by_user_ref'),
+                //retorna somente a ref
+                query.Select(
+                  'ref',
+                  //retorna o usuario pelo email
+                  query.Get(
+                    query.Match(
+                      query.Index('user_by_email'),
+                      query.Casefold(session.user.email)
                     )
+                  )
                 )
+              ),
+              query.Match(query.Index('subscription_by_status'), 'active'),
+            ])
+          )
+        )
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        }
+      } catch (error) {
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      }
+    },
+    async signIn({ user, account, profile }) {
+      const { email } = user
 
-                return true
-            } catch {
-                return false
-            }          
-        },
-    }
+      try {
+        // FQL
+        //Cria um novo usuário com email
+        await fauna.query(
+          //se o usuário não existe, cria um novo usuário, senão retorna o usuário
+          query.If(
+            // não existe o usuário com o email
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(email!)
+                )
+              )
+            ),
+            // cria um usuario
+            query.Create(query.Collection('users'), { data: { email } }),
+            // retorna o usuário
+            query.Get(
+              query.Match(query.Index('user_by_email'), query.Casefold(email!))
+            )
+          )
+        )
+
+        return true
+      } catch (error) {
+        console.log('Error in FaunaDB: ' + error)
+
+        return false
+      }
+    },
+  },
 })
