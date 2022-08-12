b32c976edfd1e97759a42ce0fe03d94a86b616fa
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-08-12 15:42:29 -0300

    feat: maxAge in next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -18,6 +18,9 @@ export default NextAuth({
     // ...add more providers here
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    maxAge: 30 * 24 * 60 * 60, // 30 days,  
+  },
   callbacks: {
     async session({ session }) {
       if (!session.user?.email) {
