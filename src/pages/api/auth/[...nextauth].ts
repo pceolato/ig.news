e2206d8e9308e24e9efb0a87a954191e5a5bde1c
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-08-12 13:01:17 -0300

    refactor: NextAuth secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,6 +6,7 @@ import GithubProvider from "next-auth/providers/github"
 import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
